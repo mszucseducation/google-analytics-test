cf24fae8163b0a317900c120573927f9808bd8c2
Monika Szucs <113265275+mszucseducation@users.noreply.github.com>
2022-12-30 18:57:36 -0800

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1TF4MX71FE"></Script>
+      <Script>
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1TF4MX71FE', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      </Script>
+      <Component {...pageProps} /> 
+    </>
+  )
 }
