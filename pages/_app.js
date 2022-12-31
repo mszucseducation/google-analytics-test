a0f4d98765fe7d35a46a686587282c11ce2d580f
Monika Szucs <113265275+mszucseducation@users.noreply.github.com>
2022-12-30 19:02:10 -0800

    updating analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,20 +5,13 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1TF4MX71FE"></Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1TF4MX71FE"></Script>
       <Script>
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1TF4MX71FE', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+
+      gtag('config', 'G-1TF4MX71FE');
       </Script>
       <Component {...pageProps} /> 
     </>
